0207df3ee9a6e39e274c40e5609ef86b4aedfed6
Meuss <thomas.miller147@gmail.com>
2020-02-27 01:02:06 +0100

    started to get some data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col>
+            <h4>My last game</h4>
+            <div>
+              <p>
+                {{ mydata.name }} <br />
+                <timeago
+                  :datetime="new Date(mydata.last_match.finished * 1000)"
+                  :auto-update="60"
+                ></timeago>
+                <br />
+                Victory? {{ mydata.last_match.victory }}
+              </p>
+              <div
+                v-for="player in mydata.last_match.players"
+                v-bind:key="player.profile_id"
+              >
+                <p>
+                  {{ player.name }}<br />
+                  Civ: {{ player.civ }}<br />
+                  <span v-if="player.rating">Rating: {{ player.rating }}</span>
+                  <span v-else>Unranked</span>
+                </p>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span
+        ><a href="https://github.com/Meuss">Meuss</a> &copy;
+        {{ new Date().getFullYear() }}</span
+      >
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  props: {
+    source: String
+  },
+
+  data: () => ({
+    drawer: null,
+    mydata: null,
+    url:
+      "https://aoe2.net/api/player/lastmatch?game=aoe2de&steam_id=76561197983320063"
+  }),
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+  methods: {
+    fetchData() {
+      fetch(this.url)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          this.mydata = data;
+        });
+    }
+  },
+  mounted() {
+    this.fetchData();
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
