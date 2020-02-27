aafa7b06c5fd409eb1dd79e8d89afac52bd8cbe6
Meuss <thomas.miller147@gmail.com>
2020-02-27 01:01:56 +0100

    vuetify and vuetimeago

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTimeago from "vue-timeago";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueTimeago, {
+  name: "Timeago",
+  locale: "en"
+});
+
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
